fcdd256e454d8042486dcfe5d0642a8552aae2af
smikhalevski <smikhalevski@gmail.com>
2022-06-30 16:39:03 +0300

    Added trieGet tests

---
+++ src/test/trieGet.test.ts
@@ -0,0 +1,19 @@
+import { Trie, trieCreate, trieGet, trieSet } from '../main';
+
+describe('trieGet', () => {
+  let trie: Trie<any>;
+
+  beforeEach(() => {
+    trie = trieCreate();
+  });
+
+  test('returns the leaf by key', () => {
+    const leaf = trieSet(trie, 'abc', 111);
+
+    expect(trieGet(trie, 'abc')).toBe(leaf);
+  });
+
+  test('returns null if key does not exist in the trie', () => {
+    expect(trieGet(trie, 'abc')).toBe(null);
+  });
+});
